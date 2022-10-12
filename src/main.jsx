2ddd347216e3455acdf9d948fd2e416dd8364507
powerm1nt <nukaworks@nuka.works>
2022-10-12 20:53:20 +0200

    :sparkles: Include app styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "@powerws/uikit/styling" // Import UiKit styling
+import "./Common/Styles/app.scss"
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById('root')).render(

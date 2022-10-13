eea1e17a453120685abb8e030c92aa835e696c1e
powerm1nt <nukaworks@nuka.works>
2022-10-13 22:05:17 +0200

    :sparkles: Added ResourcesMgmt view

---
+++ src/Views/ResourcesMgmt/ResourcesMgmt.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ResourcesMgmt () {
+  return (
+      <>
+        <div className={'Dynamic--Header'}>
+          <h1>Resources Management</h1>
+        </div>
+
+        <div className={'Dynamic--Content'}>
+          <h1>Resources Management</h1>
+        </div>
+      </>
+  )
+}

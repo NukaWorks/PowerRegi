92762d55f9b2d93745a1c5c2ee8f7a3116d64a1b
FakeSMC.kext <nukaworks@nuka.works>
2022-10-19 17:35:11 +0200

    :sparkles: Naming conventions updates

--- src/Views/ErrorView/ErrorView.jsx
+++ src/Views/ErrorView/ErrorView.jsx
@@ -11,9 +11,9 @@ export default function ErrorView() {
           </div>
 
           <div className={'Dynamic__Content App__ErrorView'}>
-            <div className={'App__ErrorView--Container'}>
-              <img className={'App__ErrorView--ExclamationIcon'} alt={"ExclamationIcon"} src={ExclamationIcon} />
-              <div className={'App__ErrorView--Intro'}>
+            <div className={'App__ErrorView--Message'}>
+              <img className={'App__ErrorView--Message__ExclamationIcon'} alt={"ExclamationIcon"} src={ExclamationIcon} />
+              <div className={'App__ErrorView--Message__Intro'}>
                 <h1>Unfortunately</h1>
                 <h4>{ commercial_name } was crashed and need to be reloaded.</h4>
               </div>

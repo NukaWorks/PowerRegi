f4fc37a0a9334d6e872e46e5144f5ed7e869136b
FakeSMC.kext <nukaworks@nuka.works>
2022-10-20 20:44:35 +0200

    :sparkles: Added WebUI Prod Server

---
+++ src/Server/webui.js
@@ -0,0 +1,27 @@
+/*
+ * PowerRegi WebUI Server
+ */
+
+const { preview } = require('vite')
+require('dotenv').config()
+
+async function startServer() {
+  const previewServer = await preview({
+    // any valid user config options, plus `mode` and `configFile`
+    preview: {
+      port: process.env.APP_WEBUI_PORT,
+      open: false,
+    }
+  })
+
+  previewServer.printUrls()
+}
+
+startServer()
+    .then(() => {
+      console.log(`WebUi is started at port ${process.env.APP_WEBUI_PORT}`)
+    })
+    .catch(err => {
+      console.error(err)
+      process.exit(1)
+    })

610f4b7bcfa54524736d13cc2d2d43279ffa001d
FakeSMC.kext <nukaworks@nuka.works>
2022-10-20 16:18:17 +0200

    :sparkles: Added InitView

---
+++ src/Views/InitView/InitView.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './InitView.scss'
+
+export default function HomeView() {
+  return (
+      <>
+        <div className={'Dynamic__Header'}>
+          <h1>Overview</h1>
+        </div>
+
+        <div className={'Dynamic__Content App__InitView'}>
+            <p>Home View</p>
+        </div>
+      </>
+  )
+}

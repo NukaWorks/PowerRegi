2f1ee1626fbdde18144010071720a0c30c57c5e4
FakeSMC.kext <nukaworks@nuka.works>
2022-10-20 20:45:30 +0200

    :sparkles: Backend Server now boot

--- src/Server/server.js
+++ src/Server/server.js
@@ -9,5 +9,16 @@ const express = require('express');
 // Setup dotenv & express
 require('dotenv').config()
 const app = express();
+const port = process.env.APP_API_PORT;
 
-console.log(process.env)
+// API Routes
+app.get('/', (req, res) => {
+  res.contentType('application/json');
+
+})
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
+

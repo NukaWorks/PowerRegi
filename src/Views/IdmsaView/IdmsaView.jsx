d3e1d237061685abe859acaba2735289f14acd51
FakeSMC.kext <nukaworks@nuka.works>
2022-10-22 18:17:52 +0200

    :truck: Renamed to IdmsaView - LoginSystem

---
+++ src/Views/IdmsaView/IdmsaView.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './IdmsaView.scss'
+import { Spinner } from '@powerws/uikit'
+
+export default function IdmsaView() {
+  return (
+      <>
+        <div className={'Dynamic__Header'}>
+          <h1>Loading</h1>
+        </div>
+
+        <div className={'Dynamic__Content App__IdmsaView'}>
+
+        </div>
+      </>
+  )
+}

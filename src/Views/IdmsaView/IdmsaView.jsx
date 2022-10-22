8121d586fa0a2161f416dd66df859d8955d027b7
FakeSMC.kext <nukaworks@nuka.works>
2022-10-22 22:55:00 +0200

    :construction: IdmsaUi

--- src/Views/IdmsaView/IdmsaView.jsx
+++ src/Views/IdmsaView/IdmsaView.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './IdmsaView.scss'
-import { Button } from '@powerws/uikit'
+import { Button, Link, TextField } from '@powerws/uikit'
 
 export default function IdmsaView() {
   return (
       <>
         <div className={'Dynamic__Header'}>
-          <h1>Loading</h1>
+          <h1>Login</h1>
         </div>
 
         <div className={'Dynamic__Content App__IdmsaView'}>
-          <Button>Click me</Button>
+          <h1 className={'App__IdmsaView--IdmsaUi__Title'}>Hi ! 👋</h1>
+          <TextField type={'text'} placeholder={'Username'} />
+          <TextField type={'password'} placeholder={'Password'} />
+          <Link href={'#'}>I forgot</Link>
+          <Button>Login</Button>
         </div>
       </>
   )

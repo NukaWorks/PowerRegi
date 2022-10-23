5bcdb9c0a1b8a1e43108bb60aa2e7386a0e6d5c1
FakeSMC.kext <nukaworks@nuka.works>
2022-10-23 12:01:55 +0200

    :sparkles: Added footer

--- src/Views/IdmsaView/IdmsaView.jsx
+++ src/Views/IdmsaView/IdmsaView.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './IdmsaView.scss'
 import { Button, Link, TextField } from '@powerws/uikit'
-
+import { commercial_name, version } from '../../../package.json'
 export default function IdmsaView() {
   return (
       <>
         <div className={'Dynamic__Header'}>
-          <h1>Idmsa Login</h1>
+          <h1>Login to PowerRegi</h1>
         </div>
 
         <div className={'Dynamic__Content App__IdmsaView'}>
@@ -36,6 +36,16 @@ export default function IdmsaView() {
                 <Link href={'#'}>I forgot my account</Link>
                 <Button color={'Primary'}>Login</Button>
               </div>
+            </div>
+
+            <div className={'App__IdmsaView--IdmsaUi__Footer'}>
+              <p className={'App__IdmsaView--IdmsaUi__Footer--InstanceLocation'}>
+                <code>regi.nuka.works</code>
+              </p>
+
+              <p className={'App__IdmsaView--IdmsaUi__Footer--AppVersion'}>
+                <code>{ commercial_name } { version }</code>
+              </p>
             </div>
           </div>
         </div>

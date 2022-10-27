f32d03bc11b92991bda1e610a31bc3eef1548ce6
FakeSMC.kext <nukaworks@nuka.works>
2022-10-27 13:06:21 +0200

    :sparkles: Imports modules

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,24 +1,28 @@
-/*
- * PowerRegi Backend Server
- */
+////////////////////////////////
+//
+// PowerRegi Backend Server
+//
+////////////////////////////////
 
 // Importing modules
 const express = require('express');
-
+const chalk = require('chalk')
+const { commercial_name, version } = require('../../package.json')
+const idmsa = require('./Idmsa/IdmsaHandler')
 
 // Setup dotenv & express
 require('dotenv').config()
 const app = express();
 const port = process.env.APP_API_PORT;
 
-// API Routes
-app.get('/', (req, res) => {
-  res.contentType('application/json');
+// Setup Logging
+const log = require('electron-log').scope('main')
 
-})
+// Define Routes
+app.use('/idmsa', idmsa)
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  log.info(`${chalk.cyanBright.bold(commercial_name)} (${chalk.bold(version)}) listening on port ${port}`)
 })
 

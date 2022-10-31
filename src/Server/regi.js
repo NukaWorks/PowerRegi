4099830210a5036f131371c7b740af1f82623105
FakeSMC.kext <nukaworks@nuka.works>
2022-10-31 23:23:53 +0100

    :sparkles: Returns version and others information on /

--- src/Server/regi.js
+++ src/Server/regi.js
@@ -14,12 +14,23 @@ const idmsa = require('./Idmsa/IdmsaHandler')
 require('dotenv').config()
 const app = express();
 const port = process.env.APP_API_PORT;
+app.disable('x-powered-by');
 
 // Setup Logging
 const log = require('electron-log').scope('main')
 
 // Define Routes
 app.use('/idmsa', idmsa)
+
+app.get('/', (req, res) => {
+  res.send({
+    'build': {
+      'name': commercial_name,
+      'version': version
+    },
+    'domain': process.env.APP_FQDN
+  })
+})
 
 // Start server
 app.listen(port, () => {

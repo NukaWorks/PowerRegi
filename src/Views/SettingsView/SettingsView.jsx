7cd18695bf431b132be0ae71d4fc65aeb57864c6
FakeSMC.kext <nukaworks@nuka.works>
2022-10-31 19:05:08 +0100

    :sparkles: Added SettingsView.jsx

---
+++ src/Views/SettingsView/SettingsView.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function SettingsView () {
+  return (
+      <>
+        <div className={'Dynamic--Header App__SettingsView'}>
+          <h1>Settings</h1>
+        </div>
+
+        <div className={'Dynamic--Content App__SettingsView'}>
+          <p>Settings</p>
+        </div>
+      </>
+  )
+}

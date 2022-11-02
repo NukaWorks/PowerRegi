0819d8c157462145800d13ca154bcdfffc86b8fe
FakeSMC.kext <nukaworks@nuka.works>
2022-11-02 18:43:30 +0100

    :sparkles: ConfigProvider: Added support for defaultConfig

--- src/Common/Misc/ConfigProvider.js
+++ src/Common/Misc/ConfigProvider.js
@@ -1,4 +1,9 @@
 import fs from 'fs'
+import os from 'os'
+import logger from 'electron-log'
+import chalk from 'chalk'
+
+const log = logger.scope('main')
 
 let instance = null
 const popularDirs = [
@@ -7,18 +12,36 @@ const popularDirs = [
   'dist/'
 ]
 
+const defaultConfig = {
+  'APP_HOST': os.hostname(),
+  'APP_WEBUI_PORT': 8080,
+  'APP_API_PORT': 8081
+}
+
 function fetchConfig() {
+  let runningConfig = {}
+
   for (const dir of popularDirs) {
     if (fs.existsSync(dir + '.env.json')) {
-      return JSON.parse(fs.readFileSync(dir + '.env.json', 'utf8'))
+      runningConfig = JSON.parse(fs.readFileSync(dir + '.env.json', 'utf8'))
     }
   }
+
+  for (const key in defaultConfig) {
+    if (!runningConfig.hasOwnProperty(key)) {
+      log.warn(`Config is missing key: ${chalk.white.bold(key)}, using default`)
+      runningConfig[key] = defaultConfig[key]
+    }
+  }
+
+  return runningConfig
 }
 
 class ConfigProvider {
   constructor() {
-    if (fetchConfig()) {
-      this.config = fetchConfig()
+    let currentConfig = fetchConfig()
+    if (currentConfig) {
+      this.config = currentConfig
       return this.config
     } else {
       throw new Error('No config file found')
@@ -34,5 +57,4 @@ function getConfig() {
   return instance
 }
 
-const config = getConfig()
-export default config
+export default getConfig()

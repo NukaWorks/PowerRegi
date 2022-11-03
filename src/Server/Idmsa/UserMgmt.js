8dfd63f660a512f056779813a4e4d3b230dad2a9
FakeSMC.kext <nukaworks@nuka.works>
2022-11-03 22:09:59 +0100

    :sparkles: Added UserManagement backend

---
+++ src/Server/Idmsa/UserMgmt.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import logger from 'electron-log'
+const userMgmt = express.Router()
+
+// Setup Logging
+const log = logger.scope('UserMgmt')
+
+// Middleware that is specific to this router
+userMgmt.use((req, res, next) => {
+  next()
+})
+
+// Defines Routes
+userMgmt.get('/', (req, res) => {
+  res.send('Hello World!')
+  log.debug('Hello World!')
+})
+
+module.exports = userMgmt

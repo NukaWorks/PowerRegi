95268fd9cbdddbc28ed87d9792de96261a26d64c
Powerm1nt <nukaworks@nuka.works>
2022-11-05 19:37:15 +0100

    :sparkles:Objects: User now works

--- src/Server/Database/Objects/User.js
+++ src/Server/Database/Objects/User.js
@@ -1,10 +1,20 @@
 import mongoose from 'mongoose'
 import { UserSchema } from '../Schemas/UserSchema'
 
-const User = mongoose.model('User', UserSchema)
+const UserModel = mongoose.model('User', UserSchema)
 
-async function makeDefaultUser() {}
+export class User {
+  async makeDefaultUser() {
+    const data = { name: 'root' }
 
-async function makeUser(userModel) {
+    if ( await UserModel.findOne({name: data.name})) {
+      throw new Error('User already exists')
+    } else return new UserModel(data)
+  }
 
+  async makeUser(userModel) {
+    return new UserModel(userModel)
+  }
 }
+
+export { UserModel }

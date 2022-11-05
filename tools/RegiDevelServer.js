366a556aee0655426586b69fdd5024e435ffe0a8
Powerm1nt <nukaworks@nuka.works>
2022-11-05 17:01:43 +0100

    :bug: Rewrite main function to prevents to run an older build of regi.js generated file.

--- tools/RegiDevelServer.js
+++ tools/RegiDevelServer.js
@@ -3,15 +3,38 @@
 const subp = require('child_process')
 const chalk = require('chalk')
 const log = require('electron-log').scope('Regi-DevServer')
+const esbuild = require('esbuild')
 
 const previewCmd = 'node ./.regi/regi.js'
+let previewProcess = null
+const buildParams = {
+  entryPoints: ['./src/Server/regi.js'],
+  bundle: true,
+  minify: false,
+  outfile: './.regi/regi.js',
+  platform: 'node',
+  logLevel: 'info',
+  target: 'node16',
+  watch: {
+    async onRebuild(error, result) {
+      log.info(`${chalk.bgYellowBright.bold('Reloading')} changes detected`)
+      if (error) {
+        log.error(`${chalk.bgRedBright.bold('Error')} Build failed — ${error}`)
+        previewProcess.kill('SIGKILL')
+      } else {
+        previewProcess.kill('SIGKILL')
+        previewProcess = await startPreview(previewCmd)
+      }
+    },
+  },
+}
 
 async function startPreview(cmd) {
   const ps = await subp.exec(cmd, {
     maxBuffer: 2000 * 1024,
     killSignal: 'SIGKILL',
-    env: { ...process.env, FORCE_COLOR: '1' }
-  }, (err, stdout, stderr)  => {
+    env: {...process.env, FORCE_COLOR: '1'}
+  }, (err, stdout, stderr) => {
     if (err && stderr.length >= 0) {
       console.error(`${stderr}`)
     }
@@ -25,35 +48,13 @@ async function startPreview(cmd) {
 }
 
 async function main() {
-  let preview = await startPreview(previewCmd)
-
-  require('esbuild').build({
-    entryPoints: ['./src/Server/regi.js'],
-    bundle: true,
-    minify: false,
-    outfile: './.regi/regi.js',
-    platform: 'node',
-    logLevel: 'info',
-    target: 'node16',
-    watch: {
-      async onRebuild(error, result) {
-        log.info(`${chalk.bgYellowBright.bold('Reloading')} changes detected`)
-        if (error) {
-          log.error(`${chalk.bgRedBright.bold('Error')} Build failed — ${error}`)
-          preview.kill('SIGKILL')
-        }
-        else {
-          preview.kill('SIGKILL')
-          preview = await startPreview(previewCmd)
-        }
-      },
-    },
-  }).then(result => {
+  esbuild.build(buildParams).then(() => {
+    previewProcess = startPreview(previewCmd)
     log.info(`Regi-DevServer is ${chalk.green('watching')} !`)
   })
 
   process.on('exit', () => {
-    preview.kill('SIGKILL')
+    previewProcess.kill('SIGKILL')
   })
 }
 

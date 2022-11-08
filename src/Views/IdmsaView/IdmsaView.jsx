8f020930bdcf20e8e39e51d6af01dbebae892d99
Powerm1nt <nukaworks@nuka.works>
2022-11-08 20:43:12 +0100

    :sparkles: Improved events

--- src/Views/IdmsaView/IdmsaView.jsx
+++ src/Views/IdmsaView/IdmsaView.jsx
@@ -1,6 +1,6 @@
-import React, { createRef, useEffect } from 'react'
+import React, { createRef, useEffect, useRef } from 'react'
 import './IdmsaView.scss'
-import { Button, Link, Menu, MenuItem, MenuList, TextField } from '@powerws/uikit'
+import { Button, Link, Menu, MenuItem, MenuList, TextField, Text } from '@powerws/uikit'
 import { commercial_name, version } from '../../../package.json'
 import axios from 'axios'
 import { AppEndpoints } from '../../App'
@@ -15,19 +15,22 @@ async function login(username, passwd) {
 }
 
 export default function IdmsaView(props) {
-  const [formDisabled, setFormDisabled] = React.useState(true)
-  const fldUsername = createRef()
-  const fldPassword = createRef()
+  const [formDisabled, setFormDisabled] = React.useState({type: 'partial'})
+  let fldUsername = createRef()
+  let fldPassword = createRef()
+  let submitBtn = createRef()
 
-  useEffect(() => {
-    fldUsername.current.addEventListener('input', e => {
-      if ((e.target.value.length > 0)) {
-        setFormDisabled(false)
-      } else {
-        setFormDisabled(true)
-      }
-    })
-  }, [])
+  const handleSubmit = e => {
+    setFormDisabled({type: 'all'})
+    login(fldUsername.current.value, fldPassword.current.value)
+        .then(res => {
+          setTimeout(() => setFormDisabled({type: 'none'}), 350)
+        })
+        .catch(err => {
+          setTimeout(() => setFormDisabled({type: 'none'}), 350)
+          console.error(err)
+        })
+  }
 
   return (
       <>
@@ -51,41 +54,57 @@ export default function IdmsaView(props) {
             </div>
 
             <form className={'App__IdmsaView--IdmsaUi__Content'}>
-              <h3 className={'App__IdmsaView--IdmsaUi__Content--Title'}>Login</h3>
+              <Text
+                  className={'App__IdmsaView--IdmsaUi__Content--Title'}
+                  disabled={formDisabled.type === 'all'}
+              >
+                Login
+              </Text>
               <div className={'App__IdmsaView--IdmsaUi__Content--Fields'}>
                 <TextField
                     className={'App__IdmsaView--IdmsaUi__Content--Fields__Field'}
                     type={'text'}
                     placeholder={'Username'}
+                    disabled={formDisabled.type === 'all'}
                     ref={fldUsername}
                     autoComplete={'username'}
+                    onInput={e => {
+                      if ((e.target.value.length > 0)) {
+                        setFormDisabled({type: 'none'})
+                      } else {
+                        setFormDisabled({type: 'partial'})
+                      }
+                    }}
+                    onKeyPress={e => {
+                      if (e.key === 'Enter') handleSubmit(e)
+                    }}
                 />
 
                 <TextField
                     className={'App__IdmsaView--IdmsaUi__Content--Fields__Field'}
                     type={'password'}
                     placeholder={'Password'}
-                    disabled={formDisabled}
+                    disabled={formDisabled.type === 'partial' || formDisabled.type === 'all'}
                     ref={fldPassword}
                     autoComplete={'current-password'}
+                    onKeyPress={e => {
+                      if (e.key === 'Enter') handleSubmit(e)
+                    }}
                 />
               </div>
 
               <div className={'App__IdmsaView--IdmsaUi__Content--Controls'}>
-                <Link href={'#'}>I forgot my account</Link>
+                <Link
+                    disabled={formDisabled.type === 'all'}
+                    href={'#'}
+                >
+                  I forgot my account
+                </Link>
                 <Button
+                    ref={submitBtn}
                     color={'Primary'}
-                    onClick={e => {
-                      e.target.disabled = true
-                      login(fldUsername.current.value, fldPassword.current.value)
-                          .then(res => {
-                            e.target.disabled = false
-                          })
-                          .catch(err => {
-                            e.target.disabled = false
-                            console.error(err)
-                          })
-                    }}
+                    disabled={formDisabled.type === 'all'}
+                    onClick={handleSubmit}
                 >
                   Login
                 </Button>

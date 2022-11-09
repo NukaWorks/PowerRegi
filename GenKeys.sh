6cc1948f5b662c07dc935e8f5b5e332efc1388b8
Powerm1nt <nukaworks@nuka.works>
2022-11-10 00:04:09 +0100

    :sparkles: Added tools/GenKeys for make openssl keys

--- GenKeys.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Generate a key pair for the server
-ssh-keygen -t rsa -b 4096 -m PEM -f regi.key
-# Don't add passphrase
-openssl rsa -in regi.key -pubout -outform PEM -out regi.key.pub

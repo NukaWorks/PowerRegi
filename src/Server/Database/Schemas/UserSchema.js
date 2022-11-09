849e7f1e16b940c0c7923c910aa5bcd953beded7
Powerm1nt <nukaworks@nuka.works>
2022-11-09 17:31:11 +0100

    :bulb: Cleanup comments

--- src/Server/Database/Schemas/UserSchema.js
+++ src/Server/Database/Schemas/UserSchema.js
@@ -1,11 +1,11 @@
 import { Schema } from 'mongoose'
 
 export const UserSchema = new Schema({
-    name: String, // Unique & Required
-    email: String, // Optional
-    passwd: String, // Required
-    groups: [String], // Optional
-    uid: String, // Required
+    name: String,
+    email: String,
+    passwd: String,
+    groups: [String],
+    uid: String,
     createdAt: {
         type: Date,
         default: Date.now

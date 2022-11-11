47286fb35089c47019c0e23b47cf3981bd58ae87
Powerm1nt <nukaworks@nuka.works>
2022-11-11 16:33:21 +0100

    :construction: Session Management

--- src/Server/Idmsa/IdmsaAuth.js
+++ src/Server/Idmsa/IdmsaAuth.js
@@ -8,6 +8,8 @@ import { certKeys } from '../../Common/Misc/ConfigProvider.mjs'
 import cookieParser from 'cookie-parser'
 import env from '../../Common/Misc/ConfigProvider.mjs'
 import jwt from 'jsonwebtoken'
+import { Session, SessionModel } from '../Database/Objects/Session'
+import chalk from 'chalk'
 
 const idmsa = express.Router()
 
@@ -32,6 +34,8 @@ idmsa.post('/login', (req, res) => {
           res.sendStatus(401, `User ${req.body.username} not found !`)
           log.error(`User ${req.body.username} not found !`)
         } else {
+          const user = result
+
           if (req.body.passwd && bcrypt.compareSync(req.body.passwd, result.passwd)) {
             if (req.cookies.idmsa) {
               let token = JWTHeader.concat(req.cookies.idmsa)
@@ -44,18 +48,29 @@ idmsa.post('/login', (req, res) => {
                 if (decoded.u !== result.uid && decoded.g !== result.groups) {
                   res.clearCookie('idmsa')
                   res.sendStatus(401, 'Invalid token')
-                } else {
-                  res.sendStatus(201)
                 }
               })
+
             } else {
               let token = makeAccessToken(result.uid, result.groups).replace(JWTHeader, '')
               res.cookie('idmsa', token, {
                 maxAge: env.APP_SESSION_EXPIRES * 60 * 60 * 1000,
                 sameSite: 'strict'
               })
-              res.sendStatus(201)
             }
+
+            if (req.cookies.session) {
+              SessionModel.findOne({id: req.cookies.session})
+                  .then(result => {
+                    if (!result) {
+                      res.clearCookie('session')
+                      makeSession(req, res, user)
+                    } else {
+                      res.sendStatus(201)
+                    }
+                  })
+            } else makeSession(req, res, user)
+
           } else res.sendStatus(401)
         }
       })
@@ -64,5 +79,24 @@ idmsa.post('/login', (req, res) => {
         log.error(err)
       })
 })
+
+function makeSession(req, res, user) {
+  new Session(req, user).build()
+      .then(result => {
+        log.log(`Created session ${chalk.white(result.id)} for user ${(chalk.white(req.body.username))}`)
+        res.cookie('session', result.id, {
+          maxAge: env.APP_SESSION_EXPIRES * 60 * 60 * 1000,
+          sameSite: 'strict'
+        })
+        res.sendStatus(201)
+        return true
+      })
+      .catch(err => {
+        res.sendStatus(401, 'Unable to create session')
+        log.error('Unable to create session')
+      })
+
+  return false
+}
 
 module.exports = idmsa

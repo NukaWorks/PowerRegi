0df439168775a02115ab8d8d6f27110d188f1498
Powerm1nt <nukaworks@nuka.works>
2022-11-11 14:24:05 +0100

    :construction: User auth handling

--- src/Server/Idmsa/IdmsaAuth.js
+++ src/Server/Idmsa/IdmsaAuth.js
@@ -7,6 +7,7 @@ import { makeAccessToken, UserModel } from '../Database/Objects/User'
 import { certKeys } from '../../Common/Misc/ConfigProvider.mjs'
 import cookieParser from 'cookie-parser'
 import env from '../../Common/Misc/ConfigProvider.mjs'
+import jwt from 'jsonwebtoken'
 
 const idmsa = express.Router()
 
@@ -19,26 +20,47 @@ idmsa.use((req, res, next) => {
 })
 
 idmsa.use(json())
-idmsa.use(cookieParser());
+idmsa.use(cookieParser())
 
-const JWTHeader = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+const JWTHeader = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.'
 
 // Defines Routes
 idmsa.post('/login', (req, res) => {
-  UserModel.findOne({ name: req.body.username })
+  UserModel.findOne({name: req.body.username})
       .then(result => {
-          if (!result) {
-            res.sendStatus(401)
-            log.error(`User ${req.body.username} not found !`)
-          } else {
-            if (bcrypt.compareSync(req.body.passwd, result.passwd)) {
-                let token = makeAccessToken(result.uid, result.groups).replace(JWTHeader, '')
-                result.updateOne({ tokens: { session: token } }).then(r => {
-                  res.cookie('idmsa', token, { maxAge: env.APP_SESSION_EXPIRES * 60 * 60 * 1000 })
-                  res.sendStatus(200)
-                })
+        if (!result) {
+          res.sendStatus(401)
+          log.error(`User ${req.body.username} not found !`)
+        } else {
+          if (req.body.passwd && bcrypt.compareSync(req.body.passwd, result.passwd)) {
+            if (!req.cookies.idmsa) {
+              let token = makeAccessToken(result.uid, result.groups).replace(JWTHeader, '')
+              res.cookie('idmsa', token, {
+                maxAge: env.APP_SESSION_EXPIRES * 60 * 60 * 1000,
+                sameSite: 'strict'
+              })
+              res.sendStatus(201)
+            } else {
+              let token = JWTHeader.concat(req.cookies.idmsa)
+              jwt.verify(token, certKeys.privkey, (err, decoded) => {
+                if (err) {
+                  res.clearCookie('idmsa')
+                  res.sendStatus(401, 'Invalid token')
+                }
+
+                if (
+                    decoded.u !== result.uid &&
+                    decoded.g !== result.groups
+                ) {
+                  res.clearCookie('idmsa')
+                  res.sendStatus(401, 'Invalid token')
+                } else {
+                  res.sendStatus(201)
+                }
+              })
             }
-          }
+          } else res.sendStatus(401)
+        }
       })
       .catch(err => () => {
         res.send(err)

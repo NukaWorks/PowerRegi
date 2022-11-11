87d9ffbcd5b38f3017f1046c3ce666141d70756d
Powerm1nt <nukaworks@nuka.works>
2022-11-11 14:28:47 +0100

    :art: Code Reformat

--- src/Server/Idmsa/IdmsaAuth.js
+++ src/Server/Idmsa/IdmsaAuth.js
@@ -29,18 +29,11 @@ idmsa.post('/login', (req, res) => {
   UserModel.findOne({name: req.body.username})
       .then(result => {
         if (!result) {
-          res.sendStatus(401)
+          res.sendStatus(401, `User ${req.body.username} not found !`)
           log.error(`User ${req.body.username} not found !`)
         } else {
           if (req.body.passwd && bcrypt.compareSync(req.body.passwd, result.passwd)) {
-            if (!req.cookies.idmsa) {
-              let token = makeAccessToken(result.uid, result.groups).replace(JWTHeader, '')
-              res.cookie('idmsa', token, {
-                maxAge: env.APP_SESSION_EXPIRES * 60 * 60 * 1000,
-                sameSite: 'strict'
-              })
-              res.sendStatus(201)
-            } else {
+            if (req.cookies.idmsa) {
               let token = JWTHeader.concat(req.cookies.idmsa)
               jwt.verify(token, certKeys.privkey, (err, decoded) => {
                 if (err) {
@@ -48,16 +41,20 @@ idmsa.post('/login', (req, res) => {
                   res.sendStatus(401, 'Invalid token')
                 }
 
-                if (
-                    decoded.u !== result.uid &&
-                    decoded.g !== result.groups
-                ) {
+                if (decoded.u !== result.uid && decoded.g !== result.groups) {
                   res.clearCookie('idmsa')
                   res.sendStatus(401, 'Invalid token')
                 } else {
                   res.sendStatus(201)
                 }
               })
+            } else {
+              let token = makeAccessToken(result.uid, result.groups).replace(JWTHeader, '')
+              res.cookie('idmsa', token, {
+                maxAge: env.APP_SESSION_EXPIRES * 60 * 60 * 1000,
+                sameSite: 'strict'
+              })
+              res.sendStatus(201)
             }
           } else res.sendStatus(401)
         }

5e9f06e87b439f17e634736c90c9751c08763710
Powerm1nt <nukaworks@nuka.works>
2022-11-12 20:06:14 +0100

    :truck: Moved Session function utilities and exports

--- src/Server/Database/Objects/Session.js
+++ src/Server/Database/Objects/Session.js
@@ -22,23 +22,28 @@ class Session {
     await this.#user.updateOne({$push: {sessions: this.session.id}})
     return this.session
   }
-
-  async checkSession(id, user) {
-    return await SessionModel.findOne({id}).then(async session => {
-          return await user.findOne({sessions: id})
-              .then(user => {
-                if (user) {
-                  return session.createdAt > Date.now()
-                } else return false
-              }).catch(() => false)
-        }
-    ).catch(() => false)
-  }
-
-  async deleteSession(id, user) {
-    await user.deleteOne({$pull: {sessions: id}})
-    return SessionModel.deleteOne({id})
-  }
 }
 
-export { SessionModel, Session }
+async function checkSession(id, user) {
+  return await SessionModel.findOne({id}).then(async session => {
+        return await user.findOne({sessions: id})
+            .then(user => {
+              if (user) {
+                return session.createdAt > Date.now()
+              } else return false
+            }).catch(() => false)
+      }
+  ).catch(() => false)
+}
+
+async function deleteSession(id, user) {
+  await user.deleteOne({$pull: {sessions: id}})
+  return SessionModel.deleteOne({id})
+}
+
+export {
+  SessionModel,
+  Session,
+  deleteSession,
+  checkSession
+}

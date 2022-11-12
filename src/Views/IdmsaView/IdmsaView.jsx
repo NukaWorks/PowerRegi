b506113be68a0f3eea8c41ecd0f48c8387d3c89b
Powerm1nt <nukaworks@nuka.works>
2022-11-12 20:05:44 +0100

    :fire: Moved IdmsaView AuthSystem to Idmsa

--- src/Views/IdmsaView/IdmsaView.jsx
+++ src/Views/IdmsaView/IdmsaView.jsx
@@ -1,157 +1,17 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import './IdmsaView.scss'
-import { Button, Link, Menu, MenuItem, MenuList, Text, TextField } from '@powerws/uikit'
-import { commercial_name, version } from '../../../package.json'
-import axios from 'axios'
-import { AppEndpoints } from '../../App'
-import Cookies from 'js-cookie'
-import { Navigate } from 'react-router-dom'
-import { useQuery } from '../../Common/Misc/Hooks'
+import Idmsa from '../../Common/Modules/Idmsa/Idmsa'
 
-async function login(username, passwd) {
-  if (!passwd.length > 0) {
-    throw new Error('Password is empty')
-  }
+export default function IdmsaView() {
+  return (
+      <>
+        <div className={'Dynamic__Header'}>
+          <h1>Login to PowerRegi</h1>
+        </div>
 
-  return await axios.post(
-      `${AppEndpoints.api}/idmsa/login`,
-      {username: username, passwd: passwd}
-  ).then(res => {
-    console.log(res.data)
-  })
-}
-
-export default function IdmsaView(props) {
-  const [formDisabled, setFormDisabled] = React.useState({type: 'partial', includes: ['submit']})
-  let fldUsername = useRef(null)
-  let fldPassword = useRef(null)
-  let submitBtn = useRef(null)
-  let formContent = useRef(null)
-  let query = useQuery()
-
-  useEffect(() => {
-    console.log(Cookies.get('idmsa'), Cookies.get('session'))
-  }, [])
-
-  const handleSubmit = e => {
-    fldUsername.current.value = fldUsername.current.value.trim()
-    if (!fldUsername.current.value.length > 0) return false
-
-    setFormDisabled({type: 'all'})
-    login(fldUsername.current.value, fldPassword.current.value)
-        .then(res => {
-          setTimeout(() => setFormDisabled({type: 'none', includes: []}), 350)
-        })
-        .catch(err => {
-          setTimeout(() => setFormDisabled({type: 'none', includes: []}), 350)
-
-          // Shake the form
-          setTimeout(() => {
-            formContent.current.style.animation = 'shake 0.1s cubic-bezier(.1,1.25,.95,.31)' +
-                ' infinite'
-            setTimeout(() => formContent.current.style.animation = 'none', 300)
-          }, 350)
-          console.error(err)
-        })
-  }
-
-  if (props.loggedIn) {
-    // Redirect to origin if logged in
-    let redir = query.get('redirect')
-    if (!redir) return <Navigate to={'/home'}/>
-    else return (<Navigate to={redir} replace={true}/>)
-  } else {
-    return (
-        <>
-          <div className={'Dynamic__Header'}>
-            <h1>Login to PowerRegi</h1>
-          </div>
-
-          <div className={'Dynamic__Content App__IdmsaView'}>
-            <div className={'App__IdmsaView--IdmsaUi'}>
-              <div className={'App__IdmsaView--IdmsaUi__Header'}>
-                <h1 className={'App__IdmsaView--IdmsaUi__Header--Title'}>Hi ! 👋</h1>
-                <Menu className={'App__IdmsaView--IdmsaUi__Header--HelpIcon'} title={
-                  <span className="material-symbols-rounded">
-                  help
-                </span>
-                }>
-                  <MenuList>
-                    <MenuItem>About Idmsa...</MenuItem>
-                  </MenuList>
-                </Menu>
-              </div>
-
-              <form ref={formContent} className={'App__IdmsaView--IdmsaUi__Content'}>
-                <Text
-                    className={'App__IdmsaView--IdmsaUi__Content--Title'}
-                    disabled={formDisabled.type === 'all'}
-                >
-                  Login
-                </Text>
-                <div className={'App__IdmsaView--IdmsaUi__Content--Fields'}>
-                  <TextField
-                      className={'App__IdmsaView--IdmsaUi__Content--Fields__Field'}
-                      type={'text'}
-                      placeholder={'Username'}
-                      disabled={formDisabled.type === 'all'}
-                      ref={fldUsername}
-                      autoComplete={'username'}
-                      onInput={e => {
-                        if ((e.target.value.length > 0)) {
-                          setFormDisabled({type: 'none', includes: []})
-                        } else {
-                          setFormDisabled({type: 'partial', includes: ['submit']})
-                        }
-                      }}
-                      onKeyPress={e => {
-                        if (e.key === 'Enter') handleSubmit(e)
-                      }}
-                  />
-
-                  <TextField
-                      className={'App__IdmsaView--IdmsaUi__Content--Fields__Field'}
-                      type={'password'}
-                      placeholder={'Password'}
-                      disabled={formDisabled.type === 'partial' || formDisabled.type === 'all'}
-                      ref={fldPassword}
-                      autoComplete={'current-password'}
-                      onKeyPress={e => {
-                        if (e.key === 'Enter') handleSubmit(e)
-                      }}
-                  />
-                </div>
-
-                <div className={'App__IdmsaView--IdmsaUi__Content--Controls'}>
-                  <Link
-                      disabled={formDisabled.type === 'all'}
-                      href={'#'}
-                  >
-                    I forgot my account
-                  </Link>
-                  <Button
-                      ref={submitBtn}
-                      color={'Primary'}
-                      disabled={formDisabled.type === 'all' || formDisabled.includes[0] === 'submit'}
-                      onClick={handleSubmit}
-                  >
-                    Login
-                  </Button>
-                </div>
-              </form>
-
-              <div className={'App__IdmsaView--IdmsaUi__Footer'}>
-                <code className={'App__IdmsaView--IdmsaUi__Footer--InstanceLocation'}>
-                  {props.data.domain}
-                </code>
-
-                <code className={'App__IdmsaView--IdmsaUi__Footer--AppVersion'}>
-                  {commercial_name} {version}
-                </code>
-              </div>
-            </div>
-          </div>
-        </>
-    )
-  }
+        <div className={'Dynamic__Content App__IdmsaView'}>
+          <Idmsa />
+        </div>
+      </>
+  )
 }

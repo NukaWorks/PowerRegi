dc9a8bb630be67be539cc266bcfff1ef6bfee234
Powerm1nt <nukaworks@nuka.works>
2022-11-12 12:58:14 +0100

    :sparkles: Redirect now works

--- src/Views/IdmsaView/IdmsaView.jsx
+++ src/Views/IdmsaView/IdmsaView.jsx
@@ -5,6 +5,8 @@ import { commercial_name, version } from '../../../package.json'
 import axios from 'axios'
 import { AppEndpoints } from '../../App'
 import Cookies from 'js-cookie'
+import { Navigate } from 'react-router-dom'
+import { useQuery } from '../../Common/Misc/Hooks'
 
 async function login(username, passwd) {
   if (!passwd.length > 0) {
@@ -25,6 +27,7 @@ export default function IdmsaView(props) {
   let fldPassword = useRef(null)
   let submitBtn = useRef(null)
   let formContent = useRef(null)
+  let query = useQuery()
 
   useEffect(() => {
     console.log(Cookies.get('idmsa'), Cookies.get('session'))
@@ -51,96 +54,103 @@ export default function IdmsaView(props) {
         })
   }
 
-  return (
-      <>
-        <div className={'Dynamic__Header'}>
-          <h1>Login to PowerRegi</h1>
-        </div>
+  if (props.loggedIn) {
+    // Redirect to origin if logged in
+    let redir = query.get('redirect')
+    if (!redir) return <Navigate to={'/home'}/>
+    else return (<Navigate to={redir} replace={true} />)
+  } else {
+    return (
+        <>
+          <div className={'Dynamic__Header'}>
+            <h1>Login to PowerRegi</h1>
+          </div>
 
-        <div className={'Dynamic__Content App__IdmsaView'}>
-          <div className={'App__IdmsaView--IdmsaUi'}>
-            <div className={'App__IdmsaView--IdmsaUi__Header'}>
-              <h1 className={'App__IdmsaView--IdmsaUi__Header--Title'}>Hi ! 👋</h1>
-              <Menu className={'App__IdmsaView--IdmsaUi__Header--HelpIcon'} title={
-                <span className="material-symbols-rounded">
+          <div className={'Dynamic__Content App__IdmsaView'}>
+            <div className={'App__IdmsaView--IdmsaUi'}>
+              <div className={'App__IdmsaView--IdmsaUi__Header'}>
+                <h1 className={'App__IdmsaView--IdmsaUi__Header--Title'}>Hi ! 👋</h1>
+                <Menu className={'App__IdmsaView--IdmsaUi__Header--HelpIcon'} title={
+                  <span className="material-symbols-rounded">
                   help
                 </span>
-              }>
-                <MenuList>
-                  <MenuItem>About Idmsa...</MenuItem>
-                </MenuList>
-              </Menu>
-            </div>
-
-            <form ref={formContent} className={'App__IdmsaView--IdmsaUi__Content'}>
-              <Text
-                  className={'App__IdmsaView--IdmsaUi__Content--Title'}
-                  disabled={formDisabled.type === 'all'}
-              >
-                Login
-              </Text>
-              <div className={'App__IdmsaView--IdmsaUi__Content--Fields'}>
-                <TextField
-                    className={'App__IdmsaView--IdmsaUi__Content--Fields__Field'}
-                    type={'text'}
-                    placeholder={'Username'}
-                    disabled={formDisabled.type === 'all'}
-                    ref={fldUsername}
-                    autoComplete={'username'}
-                    onInput={e => {
-                      if ((e.target.value.length > 0)) {
-                        setFormDisabled({type: 'none', includes: []})
-                      } else {
-                        setFormDisabled({type: 'partial', includes: ['submit']})
-                      }
-                    }}
-                    onKeyPress={e => {
-                      if (e.key === 'Enter') handleSubmit(e)
-                    }}
-                />
-
-                <TextField
-                    className={'App__IdmsaView--IdmsaUi__Content--Fields__Field'}
-                    type={'password'}
-                    placeholder={'Password'}
-                    disabled={formDisabled.type === 'partial' || formDisabled.type === 'all'}
-                    ref={fldPassword}
-                    autoComplete={'current-password'}
-                    onKeyPress={e => {
-                      if (e.key === 'Enter') handleSubmit(e)
-                    }}
-                />
+                }>
+                  <MenuList>
+                    <MenuItem>About Idmsa...</MenuItem>
+                  </MenuList>
+                </Menu>
               </div>
 
-              <div className={'App__IdmsaView--IdmsaUi__Content--Controls'}>
-                <Link
+              <form ref={formContent} className={'App__IdmsaView--IdmsaUi__Content'}>
+                <Text
+                    className={'App__IdmsaView--IdmsaUi__Content--Title'}
                     disabled={formDisabled.type === 'all'}
-                    href={'#'}
-                >
-                  I forgot my account
-                </Link>
-                <Button
-                    ref={submitBtn}
-                    color={'Primary'}
-                    disabled={formDisabled.type === 'all' || formDisabled.includes[0] === 'submit'}
-                    onClick={handleSubmit}
                 >
                   Login
-                </Button>
+                </Text>
+                <div className={'App__IdmsaView--IdmsaUi__Content--Fields'}>
+                  <TextField
+                      className={'App__IdmsaView--IdmsaUi__Content--Fields__Field'}
+                      type={'text'}
+                      placeholder={'Username'}
+                      disabled={formDisabled.type === 'all'}
+                      ref={fldUsername}
+                      autoComplete={'username'}
+                      onInput={e => {
+                        if ((e.target.value.length > 0)) {
+                          setFormDisabled({type: 'none', includes: []})
+                        } else {
+                          setFormDisabled({type: 'partial', includes: ['submit']})
+                        }
+                      }}
+                      onKeyPress={e => {
+                        if (e.key === 'Enter') handleSubmit(e)
+                      }}
+                  />
+
+                  <TextField
+                      className={'App__IdmsaView--IdmsaUi__Content--Fields__Field'}
+                      type={'password'}
+                      placeholder={'Password'}
+                      disabled={formDisabled.type === 'partial' || formDisabled.type === 'all'}
+                      ref={fldPassword}
+                      autoComplete={'current-password'}
+                      onKeyPress={e => {
+                        if (e.key === 'Enter') handleSubmit(e)
+                      }}
+                  />
+                </div>
+
+                <div className={'App__IdmsaView--IdmsaUi__Content--Controls'}>
+                  <Link
+                      disabled={formDisabled.type === 'all'}
+                      href={'#'}
+                  >
+                    I forgot my account
+                  </Link>
+                  <Button
+                      ref={submitBtn}
+                      color={'Primary'}
+                      disabled={formDisabled.type === 'all' || formDisabled.includes[0] === 'submit'}
+                      onClick={handleSubmit}
+                  >
+                    Login
+                  </Button>
+                </div>
+              </form>
+
+              <div className={'App__IdmsaView--IdmsaUi__Footer'}>
+                <code className={'App__IdmsaView--IdmsaUi__Footer--InstanceLocation'}>
+                  {props.data.domain}
+                </code>
+
+                <code className={'App__IdmsaView--IdmsaUi__Footer--AppVersion'}>
+                  {commercial_name} {version}
+                </code>
               </div>
-            </form>
-
-            <div className={'App__IdmsaView--IdmsaUi__Footer'}>
-              <code className={'App__IdmsaView--IdmsaUi__Footer--InstanceLocation'}>
-                {props.data.domain}
-              </code>
-
-              <code className={'App__IdmsaView--IdmsaUi__Footer--AppVersion'}>
-                {commercial_name} {version}
-              </code>
             </div>
           </div>
-        </div>
-      </>
-  )
+        </>
+    )
+  }
 }

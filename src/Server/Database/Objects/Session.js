8dd6aa4aa4522162e59d27a82697594ae32231a6
Powerm1nt <nukaworks@nuka.works>
2022-11-13 17:01:55 +0100

    :sparkles: Session: Added others utilities for manage sessions

--- src/Server/Database/Objects/Session.js
+++ src/Server/Database/Objects/Session.js
@@ -35,14 +35,57 @@ async function checkSession(sid, uid) {
   ).catch(() => false)
 }
 
-async function deleteSession(id, user) {
-  await user.deleteOne({$pull: {sessions: id}})
-  return SessionModel.deleteOne({id})
+async function deleteSession(sid, uid) {
+   return await UserModel.findOne({uid: uid}).then(user => {
+    user.deleteOne({$pull: {uid: uid, sessions: sid}})
+    return SessionModel.deleteOne({sid})
+  })
+}
+
+async function deleteAllSessions(uid) {
+   return await UserModel.findOne({uid: uid}).then(user => {
+    user.updateOne({uid: uid, sessions: []})
+     return SessionModel.deleteMany({id: {$in: user.sessions}})
+  })
+}
+
+async function deleteAllExpiredSessions(uid) {
+   return await UserModel.findOne({uid: uid}).then(user => {
+     return SessionModel.find({id: {$in: user.sessions}, expires: {$lt: new Date()}}).then(sessions => {
+       return sessions.forEach(session => {
+         user.updateOne({$pull: {sessions: session.id}})
+         session.deleteOne()
+       })
+     })
+  })
+}
+
+async function disableSession(sid, uid) {
+   return await UserModel.findOne({uid: uid}).then(user => {
+     return SessionModel.updateOne({sid: sid}, {disabled: true})
+  })
+}
+
+async function disableAllSessions(uid) {
+   return await UserModel.findOne({uid: uid}).then(user => {
+    return SessionModel.updateMany({id: {$in: user.sessions}}, {disabled: true})
+  })
+}
+
+async function disableAllExpiredSessions(uid) {
+  return await UserModel.findOne({uid: uid}).then(user => {
+    return SessionModel.updateMany({expires: {$lt: new Date()}}, {disabled: true})
+  })
 }
 
 export {
   SessionModel,
   Session,
   deleteSession,
+  deleteAllExpiredSessions,
+  deleteAllSessions,
+  disableSession,
+  disableAllSessions,
+  disableAllExpiredSessions,
   checkSession
 }

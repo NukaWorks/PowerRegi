d50fa9d370ac3ceb04e600c4a591f2f66e1a74de
Powerm1nt <nukaworks@nuka.works>
2022-11-13 12:54:52 +0100

    :sparkles: Impl AuthRoute

--- src/Common/Modules/Router/Router.jsx
+++ src/Common/Modules/Router/Router.jsx
@@ -7,30 +7,28 @@ import ConsoleMgmt from '../../../Views/ConsoleMgmt/ConsoleMgmt'
 import SettingsView from '../../../Views/SettingsView/SettingsView'
 import ErrorView from '../../../Views/ErrorView/ErrorView'
 import ErrorTypes from '../../Misc/ErrorTypes'
+import AuthRoute from '../../Misc/AuthRoute'
 
 export default function Router() {
   const authContext = React.useContext(AuthContext)
 
   return (
       <BrowserRouter>
+        <>
         <Routes>
           <Route path={'/'} element={<Navigate replace to={'/home'}/>}/>
-          <Route
-              caseSensitive
-              path="/home"
-              element={authContext.logged
-                  ? <Home/>
-                  : <Navigate
-                      replace
-                      to={`/idmsa?redirect=${encodeURIComponent(document.location.pathname)}`}
-                  />}
-          />
+          <Route path={'/home'} element={
+            <AuthRoute>
+              <Home />
+            </AuthRoute>
+          }/>
           <Route caseSensitive path={'/idmsa'} element={<IdmsaView />}/>
           <Route caseSensitive path={'/console'} element={<ConsoleMgmt/>}/>
           <Route caseSensitive path={'/settings'} element={<SettingsView/>}/>
 
           <Route path="*" element={<ErrorView errorCode={ErrorTypes['404']}/>}/>
         </Routes>
+        </>
       </BrowserRouter>
   )
 }

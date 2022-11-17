e72bb665ddcb6a9a851d5d6542e009993f1cfc9d
Powerm1nt <nukaworks@nuka.works>
2022-11-17 21:53:46 +0100

    :sparkles: AppContexts: Added TargetContext

--- src/Common/Misc/AppContexts.js
+++ src/Common/Misc/AppContexts.js
@@ -3,5 +3,6 @@ import React from 'react'
 const AuthContext = React.createContext()
 const DataContext = React.createContext()
 const StateContext = React.createContext()
+const TargetContext = React.createContext()
 
-export { AuthContext, DataContext, StateContext }
+export { AuthContext, DataContext, StateContext, TargetContext }

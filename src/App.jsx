f5bb7264ed568be0842030028f1dbce9df7cb868
Powerm1nt <nukaworks@nuka.works>
2022-11-17 13:43:53 +0100

    :art: Code Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,9 @@ import ErrorView from './Views/ErrorView/ErrorView'
 import ErrorTypes from './Common/Misc/ErrorTypes'
 import axios from 'axios'
 import { AuthContext, StateContext } from './Common/Misc/AppContexts'
-import Router from './Common/Modules/Router/Router'
+import AppRouter from './Common/Modules/Router/AppRouter'
 import { DataContext } from './Common/Misc/AppContexts'
 import StatusOverlay from './Common/Modules/StatusOverlay/StatusOverlay'
-import { BrowserRouter } from 'react-router-dom'
-
 
 export const AppConfig = {
   APP_HOST: window.location.hostname,
@@ -54,65 +52,65 @@ export default function App() {
         <DataContext.Provider value={{data, setData}}>
           <AuthContext.Provider value={{logged, setLogged}}>
             <AppActivity theme={'Light'}>
-              <BrowserRouter>
-                <AppHeader title={commercial_name}>
-                  <MenuBar>
-                    <Menu title={'File'}>
-                      <MenuList>
-                        <MenuItem>New Repository</MenuItem>
-                        <MenuItem>New Application</MenuItem>
-                        <MenuItem>New Package</MenuItem>
-                        <MenuItem>Settings</MenuItem>
-                        <MenuItem
-                            disabled={logged}
-                            onClick={() => window.location = `/idmsa?logout=true&redirect=${encodeURIComponent(document.location.pathname)}`}>
-                          Logout...
-                        </MenuItem>
-                      </MenuList>
-                    </Menu>
+              <AppHeader title={commercial_name}>
+                <MenuBar>
+                  <Menu title={'File'}>
+                    <MenuList>
+                      <MenuItem>New Repository</MenuItem>
+                      <MenuItem>New Application</MenuItem>
+                      <MenuItem>New Package</MenuItem>
+                      <MenuItem>Settings</MenuItem>
+                      <MenuItem
+                          disabled={logged}
+                          onClick={() => window.location = `/idmsa?logout=true&redirect=${encodeURIComponent(document.location.pathname)}`}>
+                        Logout...
+                      </MenuItem>
+                    </MenuList>
+                  </Menu>
 
-                    <Menu title={'Tools'}>
-                      <MenuList>
-                        <MenuItem onClick={() => window.location = '/home'}>Go to Home
-                          View</MenuItem>
-                        <MenuItem onClick={() => window.location.reload()}>Reload</MenuItem>
-                      </MenuList>
-                    </Menu>
+                  <Menu title={'Tools'}>
+                    <MenuList>
+                      <MenuItem onClick={() => window.location = '/home'}>Go to Home
+                        View</MenuItem>
+                      <MenuItem onClick={() => window.location.reload()}>Reload</MenuItem>
+                    </MenuList>
+                  </Menu>
 
-                    <Menu title={'Help'}>
-                      <MenuList>
-                        <MenuItem>Check for Updates...</MenuItem>
-                        <MenuItem>Documentation Center</MenuItem>
-                        <MenuItem>About PowerWs & UiKit</MenuItem>
-                        <MenuItem>About {commercial_name}</MenuItem>
-                      </MenuList>
-                    </Menu>
-                  </MenuBar>
-                </AppHeader>
-                {applicationState.state === 'loading' ? (
-                    <div className={'App__LoadingScreen'}>
-                      <Spinner size={'Large'} color={'Blue'}/>
-                    </div>
-                ) : (
-                    <VBox>
-                      <Sidebar>
-                        <SidebarItem icon={'home'} text={'Home'}/>
-                        <SidebarItem icon={'inventory_2'} text={'Packages'}/>
-                        <SidebarItem icon={'terminal'} text={'Console Management'}/>
-                        <SidebarItem icon={'settings'} text={'Settings'}/>
-                        <SidebarItem icon={'info'} text={'About'}/>
-                      </Sidebar>
+                  <Menu title={'Help'}>
+                    <MenuList>
+                      <MenuItem>Check for Updates...</MenuItem>
+                      <MenuItem>Documentation Center</MenuItem>
+                      <MenuItem>About PowerWs & UiKit</MenuItem>
+                      <MenuItem>About {commercial_name}</MenuItem>
+                    </MenuList>
+                  </Menu>
+                </MenuBar>
+              </AppHeader>
+              {applicationState.state === 'loading' ? (
+                  <div className={'App__LoadingScreen'}>
+                    <Spinner size={'Large'} color={'Blue'}/>
+                  </div>
+              ) : (
+                  <VBox>
+                    {applicationState.state !== 'crashed' ? (
+                        <Sidebar>
+                          <SidebarItem icon={'home'} text={'Home'}/>
+                          <SidebarItem icon={'inventory_2'} text={'Packages'}/>
+                          <SidebarItem icon={'terminal'} text={'Console Management'}/>
+                          <SidebarItem icon={'settings'} text={'Settings'}/>
+                          <SidebarItem icon={'info'} text={'About'}/>
+                        </Sidebar>
+                    ) : <></>}
 
-                      <UiApp rounded>
-                        {applicationState.state === 'crashed' ? (
-                            <ErrorView errorCode={ErrorTypes['500']}/>
-                        ) : (
-                            <Router data={data}/>
-                        )}
-                      </UiApp>
-                    </VBox>
-                )}
-              </BrowserRouter>
+                    <UiApp rounded>
+                      {applicationState.state === 'crashed' ? (
+                          <ErrorView errorCode={ErrorTypes['500']}/>
+                      ) : (
+                          <AppRouter data={data}/>
+                      )}
+                    </UiApp>
+                  </VBox>
+              )}
               <StatusOverlay/>
             </AppActivity>
           </AuthContext.Provider>

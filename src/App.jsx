d37e38fc7ce4175d6fb97e9a75a5271346d4f245
Powerm1nt <nukaworks@nuka.works>
2022-11-17 21:54:24 +0100

    :sparkles: Impl TargetContext & :trunk: Impl Loading Overlay

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,11 @@ import React, { useEffect } from 'react'
 import ErrorView from './Views/ErrorView/ErrorView'
 import ErrorTypes from './Common/Misc/ErrorTypes'
 import axios from 'axios'
-import { AuthContext, StateContext } from './Common/Misc/AppContexts'
+import { AuthContext, StateContext, TargetContext } from './Common/Misc/AppContexts'
 import AppRouter from './Common/Modules/Router/AppRouter'
 import { DataContext } from './Common/Misc/AppContexts'
 import StatusOverlay from './Common/Modules/StatusOverlay/StatusOverlay'
+import LoadingOverlay from './Common/Modules/LoadingOverlay/LoadingOverlay'
 
 export const AppConfig = {
   APP_HOST: window.location.hostname,
@@ -31,6 +32,7 @@ export const AppEndpoints = {
 export default function App() {
   // Available applicationStates: 'loading', 'crashed', 'done'.
   const [applicationState, setApplicationState] = React.useState({state: 'loading'})
+  const [targetState, setTargetState] = React.useState({target: ''})
   const [data, setData] = React.useState({})
   const [logged, setLogged] = React.useState(false)
 
@@ -49,72 +51,81 @@ export default function App() {
 
   return (
       <StateContext.Provider value={{applicationState, setApplicationState}}>
-        <DataContext.Provider value={{data, setData}}>
-          <AuthContext.Provider value={{logged, setLogged}}>
-            <AppActivity theme={'Light'}>
-              <AppHeader title={commercial_name}>
-                <MenuBar>
-                  <Menu title={'File'}>
-                    <MenuList>
-                      <MenuItem>New Repository</MenuItem>
-                      <MenuItem>New Application</MenuItem>
-                      <MenuItem>New Package</MenuItem>
-                      <MenuItem>Settings</MenuItem>
-                      <MenuItem
-                          disabled={logged}
-                          onClick={() => window.location = `/idmsa?logout=true&redirect=${encodeURIComponent(document.location.pathname)}`}>
-                        Logout...
-                      </MenuItem>
-                    </MenuList>
-                  </Menu>
+        <TargetContext.Provider value={{targetState, setTargetState}}>
+          <DataContext.Provider value={{data, setData}}>
+            <AuthContext.Provider value={{logged, setLogged}}>
+              <AppActivity theme={'Light'}>
+                <AppHeader title={commercial_name}>
+                  <MenuBar>
+                    <Menu title={'File'}>
+                      <MenuList>
+                        <MenuItem>New Repository</MenuItem>
+                        <MenuItem>New Application</MenuItem>
+                        <MenuItem>New Package</MenuItem>
+                        <MenuItem>Settings</MenuItem>
+                        <MenuItem
+                            disabled={logged}
+                            onClick={() => window.location = `/idmsa?logout=true&redirect=${encodeURIComponent(document.location.pathname)}`}>
+                          Logout...
+                        </MenuItem>
+                      </MenuList>
+                    </Menu>
 
-                  <Menu title={'Tools'}>
-                    <MenuList>
-                      <MenuItem onClick={() => window.location = '/home'}>Go to Home
-                        View</MenuItem>
-                      <MenuItem onClick={() => window.location.reload()}>Reload</MenuItem>
-                    </MenuList>
-                  </Menu>
+                    <Menu title={'Tools'}>
+                      <MenuList>
+                        <MenuItem onClick={() => window.location = '/home'}>Go to Home
+                          View</MenuItem>
+                        <MenuItem onClick={() => window.location.reload()}>Reload</MenuItem>
+                      </MenuList>
+                    </Menu>
 
-                  <Menu title={'Help'}>
-                    <MenuList>
-                      <MenuItem>Check for Updates...</MenuItem>
-                      <MenuItem>Documentation Center</MenuItem>
-                      <MenuItem>About PowerWs & UiKit</MenuItem>
-                      <MenuItem>About {commercial_name}</MenuItem>
-                    </MenuList>
-                  </Menu>
-                </MenuBar>
-              </AppHeader>
-              {applicationState.state === 'loading' ? (
-                  <div className={'App__LoadingScreen'}>
-                    <Spinner size={'Large'} color={'Blue'}/>
-                  </div>
-              ) : (
-                  <VBox>
-                    {applicationState.state !== 'crashed' ? (
-                        <Sidebar>
-                          <SidebarItem icon={'home'} text={'Home'}/>
-                          <SidebarItem icon={'inventory_2'} text={'Packages'}/>
-                          <SidebarItem icon={'terminal'} text={'Console Management'}/>
-                          <SidebarItem icon={'settings'} text={'Settings'}/>
-                          <SidebarItem icon={'info'} text={'About'}/>
-                        </Sidebar>
-                    ) : <></>}
+                    <Menu title={'Help'}>
+                      <MenuList>
+                        <MenuItem>Check for Updates...</MenuItem>
+                        <MenuItem>Documentation Center</MenuItem>
+                        <MenuItem>About PowerWs & UiKit</MenuItem>
+                        <MenuItem>About {commercial_name}</MenuItem>
+                      </MenuList>
+                    </Menu>
+                  </MenuBar>
+                </AppHeader>
+                {applicationState.state === 'loading' ? (
+                    <LoadingOverlay />
+                ) : (
+                    <VBox>
+                      {applicationState.state !== 'crashed' ? (
+                          <Sidebar>
+                            <SidebarItem icon={'home'} text={'Home'}
+                                         active={targetState.target === '/home'}
+                                         onClick={() => setTargetState({target: '/home'})}/>
+                            <SidebarItem icon={'inventory_2'} text={'Packages'}
+                                         active={targetState.target === '/console/packages'}
+                                         onClick={() => setTargetState({target: '/console/packages'})}/>
+                            <SidebarItem icon={'terminal'} text={'Console Management'}
+                                         active={targetState.target === '/console'}
+                                         onClick={() => setTargetState({target: '/console'})}/>
+                            <SidebarItem icon={'settings'} text={'Settings'}
+                                         active={targetState.target === '/settings'}
+                                         onClick={() => setTargetState({target: '/settings'})}/>
+                            <SidebarItem icon={'info'} text={'About'}
+                                         active={targetState.target === '/settings/about'}
+                                         onClick={() => setTargetState({target: '/settings/about'})}/>
+                          </Sidebar>
+                      ) : <></>}
 
-                    <UiApp rounded>
-                      {applicationState.state === 'crashed' ? (
-                          <ErrorView errorCode={ErrorTypes['500']}/>
-                      ) : (
-                          <AppRouter data={data}/>
-                      )}
-                    </UiApp>
-                  </VBox>
-              )}
-              <StatusOverlay/>
-            </AppActivity>
-          </AuthContext.Provider>
-        </DataContext.Provider>
+                      <UiApp rounded>
+                        {applicationState.state === 'crashed' ? (
+                                <ErrorView errorCode={ErrorTypes['500']}/>)
+                            : (<></>)}
+                        <AppRouter data={data}/>
+                      </UiApp>
+                    </VBox>
+                )}
+                <StatusOverlay/>
+              </AppActivity>
+            </AuthContext.Provider>
+          </DataContext.Provider>
+        </TargetContext.Provider>
       </StateContext.Provider>
   )
 }

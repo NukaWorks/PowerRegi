a913a3917ce26dba6a3adf37055bcc9765955e35
Powerm1nt <nukaworks@nuka.works>
2022-11-20 15:46:19 +0100

    :art: Code Cleanup

--- src/Views/SettingsView/SettingsView.jsx
+++ src/Views/SettingsView/SettingsView.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function SettingsView () {
   return (
       <>
-        <div className={'Dynamic--Header App__SettingsView'}>
+        <div className={'Dynamic__Header'}>
           <h1>Settings</h1>
         </div>
 
-        <div className={'Dynamic--Content App__SettingsView'}>
-          <p>Settings</p>
+        <div className={'Dynamic__Content App__SettingsView'}>
+          <p>Settings Center</p>
         </div>
       </>
   )

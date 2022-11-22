da0facea709cd2a65b46bcf1b1c732e7115913cb
Powerm1nt <nukaworks@nuka.works>
2022-11-22 18:22:04 +0100

    :bug: Fixed infinite loop while getting a 404 page on appl startup

--- src/Common/Modules/Idmsa/AuthRoute.jsx
+++ src/Common/Modules/Idmsa/AuthRoute.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react'
 import { Navigate, useNavigate } from 'react-router-dom'
-import { AuthContext, TargetContext } from '../../Misc/AppContexts'
+import { AuthContext, StateContext, TargetContext } from '../../Misc/AppContexts'
 
 export default function AuthRoute({children, element, enforceAuth}) {
   const authContext = React.useContext(AuthContext)
   const {targetState, setTargetState} = React.useContext(TargetContext)
-
   const navigate = useNavigate()
 
   useEffect(() => {
-    if (window.location.pathname !== targetState.target) {
+    if (targetState.target && window.location.pathname !== targetState.target) {
       navigate(targetState.target)
       setTargetState({target: window.location.pathname})
     }
@@ -20,8 +19,8 @@ export default function AuthRoute({children, element, enforceAuth}) {
   } else {
     return (
         <Navigate
-          replace
-          to={`/idmsa?redirect=${encodeURIComponent(document.location.pathname)}`}
+            replace
+            to={`/idmsa?redirect=${encodeURIComponent(document.location.pathname)}`}
         />
     )
   }

b768379b1a329f0c82408d0db471a579ae36055b
Powerm1nt <nukaworks@nuka.works>
2022-11-27 16:33:04 +0100

    🐛 Fixed process switching

--- tools/RegiDevelServer.mjs
+++ tools/RegiDevelServer.mjs
@@ -6,18 +6,22 @@ import { stdin, stdout } from 'node:process'
 import esbuild from 'esbuild'
 
 const log = logger.scope('Regi-DevServer')
-
 const rl = readline.createInterface(stdin, stdout)
-const previewCmd = 'node ./.regi/regi.js'
+const previewCmd = {
+  cmd: 'node',
+  args: ['regi']
+}
 const psParams = {
+  cwd: './.regi',
   maxBuffer: 2000 * 1024,
-  killSignal: 'SIGTERM',
+  detached: false,
+  killSignal: 'SIGKILL',
   env: {...process.env, FORCE_COLOR: '1'}
 }
 let previewProcess = {}
 
-async function startPreview(cmd) {
-  const ps = await subp.exec(cmd, psParams)
+async function startPreview(cmds) {
+  const ps = await subp.spawn(cmds.cmd, cmds.args, psParams)
 
   ps.stdout.on('data', data => process.stdout.write(data))
   ps.stderr.on('data', data => process.stderr.write(data))
@@ -43,16 +47,20 @@ async function main() {
         if (error) {
           isCrashed = true
           log.error(`${chalk.bgRedBright.bold('Error')} Build failed — ${error}`)
-          await previewProcess.ps.kill('SIGTERM')
+          await previewProcess.ps.kill('SIGKILL')
           if (!isCrashed) {
             await startPreview(previewCmd).then(ps => {
               previewProcess.ps = ps
+            }).catch(err => {
+              log.error(`${chalk.bgRedBright.bold('Error')} ${err}`)
             })
           }
         } else {
-          await previewProcess.ps.kill('SIGTERM')
+          await previewProcess.ps.kill('SIGKILL')
           await startPreview(previewCmd).then(ps => {
             previewProcess.ps = ps
+          }).catch(err => {
+            log.error(`${chalk.bgRedBright.bold('Error')} ${err}`)
           })
         }
       },
@@ -62,10 +70,12 @@ async function main() {
   await esbuild.build(buildParams).then(async () => {
     await startPreview(previewCmd).then(ps => {
       previewProcess.ps = ps
+    }).catch(err => {
+      log.error(`${chalk.bgRedBright.bold('Error')} ${err}`)
     })
 
     process.on('exit', () => {
-      previewProcess.ps.kill('SIGTERM')
+      previewProcess.ps.kill('SIGKILL')
     })
 
     rl.on('line', async (input) => {
@@ -73,10 +83,12 @@ async function main() {
         if (isCrashed) {
           log.error(`${chalk.bgRedBright.bold('Error')} Please fix errors before reloading`)
         } else {
-          await previewProcess.ps.kill('SIGTERM')
-          await startPreview(previewCmd).then(ps => {
+          await previewProcess.ps.kill('SIGKILL')
+          startPreview(previewCmd).then(ps => {
             previewProcess.ps = ps
             log.info(`${chalk.bgGreenBright.bold('Restarted')} !`)
+          }).catch(err => {
+            log.error(`${chalk.bgRedBright.bold('Error')} ${err}`)
           })
         }
       }

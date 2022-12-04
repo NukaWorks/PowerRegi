bcb8cabc14bdd2bb509bdbccbd0eb78a1c6f6c2b
Chabot Leonard <nukaworks@nuka.works>
2022-12-04 17:03:44 +0100

    :bug: Using uuid instead crypto package, and resolved uuid errors

--- src/Server/Database/Objects/Session.js
+++ src/Server/Database/Objects/Session.js
@@ -1,91 +1,92 @@
 import mongoose from 'mongoose'
-import { SessionSchema } from '../Schemas/SessionSchema'
+import {SessionSchema} from '../Schemas/SessionSchema'
 import env from '../../../Common/Misc/ConfigProvider.mjs'
-import { UserModel } from './User'
+import {UserModel} from './User'
+import { v4 as uuidv4 } from 'uuid'
 
 const SessionModel = mongoose.model('Session', SessionSchema)
 
 class Session {
-  #user
+   #user
 
-  constructor(req, user) {
-    this.#user = user
-    this.session = new SessionModel({
-      id: crypto.randomUUID().replaceAll('-', ''),
-      ip: req.ip,
-      ua: req.headers['user-agent'],
-      expires: new Date(Date.now() + (env.APP_SESSION_EXPIRES * 60 * 60 * 1000))
-    })
-  }
+   constructor(req, user) {
+      this.#user = user
+      this.session = new SessionModel({
+         id: uuidv4().replaceAll('-', ''),
+         ip: req.ip,
+         ua: req.headers['user-agent'],
+         expires: new Date(Date.now() + (env.APP_SESSION_EXPIRES * 60 * 60 * 1000))
+      })
+   }
 
-  async build() {
-    await this.session.save()
-    await this.#user.updateOne({$push: {sessions: this.session.id}})
-    return this.session
-  }
+   async build() {
+      await this.session.save()
+      await this.#user.updateOne({$push: {sessions: this.session.id}})
+      return this.session
+   }
 }
 
 async function checkSession(sid, uid) {
-  return await SessionModel.findOne({sid}).then(async session => {
-        return await UserModel.findOne({uid: uid, sessions: sid})
-            .then(user => {
-              return !!user;
-            }).catch(() => false)
-      }
-  ).catch(() => false)
+   return await SessionModel.findOne({sid}).then(async session => {
+          return await UserModel.findOne({uid: uid, sessions: sid})
+              .then(user => {
+                 return !!user;
+              }).catch(() => false)
+       }
+   ).catch(() => false)
 }
 
 async function deleteSession(sid, uid) {
    return await UserModel.findOne({uid: uid}).then(user => {
-    user.deleteOne({$pull: {uid: uid, sessions: sid}})
-    return SessionModel.deleteOne({sid})
-  })
+      user.deleteOne({$pull: {uid: uid, sessions: sid}})
+      return SessionModel.deleteOne({sid})
+   })
 }
 
 async function deleteAllSessions(uid) {
    return await UserModel.findOne({uid: uid}).then(user => {
-    user.updateOne({uid: uid, sessions: []})
-     return SessionModel.deleteMany({id: {$in: user.sessions}})
-  })
+      user.updateOne({uid: uid, sessions: []})
+      return SessionModel.deleteMany({id: {$in: user.sessions}})
+   })
 }
 
 async function deleteAllExpiredSessions(uid) {
    return await UserModel.findOne({uid: uid}).then(user => {
-     return SessionModel.find({id: {$in: user.sessions}, expires: {$lt: new Date()}}).then(sessions => {
-       return sessions.forEach(session => {
-         user.updateOne({$pull: {sessions: session.id}})
-         session.deleteOne()
-       })
-     })
-  })
+      return SessionModel.find({id: {$in: user.sessions}, expires: {$lt: new Date()}}).then(sessions => {
+         return sessions.forEach(session => {
+            user.updateOne({$pull: {sessions: session.id}})
+            session.deleteOne()
+         })
+      })
+   })
 }
 
 async function disableSession(sid, uid) {
    return await UserModel.findOne({uid: uid}).then(user => {
-     return SessionModel.updateOne({id: sid}, {disabled: true})
-  })
+      return SessionModel.updateOne({id: sid}, {disabled: true})
+   })
 }
 
 async function disableAllSessions(uid) {
    return await UserModel.findOne({uid: uid}).then(user => {
-    return SessionModel.updateMany({id: {$in: user.sessions}}, {disabled: true})
-  })
+      return SessionModel.updateMany({id: {$in: user.sessions}}, {disabled: true})
+   })
 }
 
 async function disableAllExpiredSessions(uid) {
-  return await UserModel.findOne({uid: uid}).then(user => {
-    return SessionModel.updateMany({expires: {$lt: new Date()}}, {disabled: true})
-  })
+   return await UserModel.findOne({uid: uid}).then(user => {
+      return SessionModel.updateMany({expires: {$lt: new Date()}}, {disabled: true})
+   })
 }
 
 export {
-  SessionModel,
-  Session,
-  deleteSession,
-  deleteAllExpiredSessions,
-  deleteAllSessions,
-  disableSession,
-  disableAllSessions,
-  disableAllExpiredSessions,
-  checkSession
+   SessionModel,
+   Session,
+   deleteSession,
+   deleteAllExpiredSessions,
+   deleteAllSessions,
+   disableSession,
+   disableAllSessions,
+   disableAllExpiredSessions,
+   checkSession
 }
--- src/Server/Database/Objects/User.js
+++ src/Server/Database/Objects/User.js
@@ -1,67 +1,67 @@
 import mongoose from 'mongoose'
 import bcrypt from 'bcryptjs'
-import crypto from 'crypto'
-import { UserSchema } from '../Schemas/UserSchema'
+import { v4 as uuidv4 } from 'uuid'
+import {UserSchema} from '../Schemas/UserSchema'
 import jwt from 'jsonwebtoken'
-import env, { certKeys } from '../../../Common/Misc/ConfigProvider.mjs'
+import env, {certKeys} from '../../../Common/Misc/ConfigProvider.mjs'
 
 const saltRounds = 10
 const UserModel = mongoose.model('User', UserSchema)
 
 //NOTE: A user need to be associated with a group to be able to login (users)
 class User {
-  constructor(user) {
-    this.model = user
-  }
+   constructor(user) {
+      this.model = user
+   }
 
-  async generateHash(password) {
-    return await bcrypt.hash(password, saltRounds)
-  }
+   async generateHash(password) {
+      return await bcrypt.hash(password, saltRounds)
+   }
 
-  #makeUid() {
-    return crypto.randomUUID().split('-')[0]
-  }
+   #makeUid() {
+      return uuidv4().split('-')[0]
+   }
 
-  async build() {
-    const uModel = await UserModel.findOne({name: this.model.name})
-    if (!uModel) {
-      this.model = new UserModel(this.model)
-      this.model.uid = this.#makeUid()
-      this.model.passwd = await this.generateHash(this.model.passwd)
-      if (!this.model.groups.includes('users')) this.model.groups.push('users')
-      return this.model.save().then(model => {
-        return model
-      })
-    } else {
-      this.model = uModel
-    }
-  }
+   async build() {
+      const uModel = await UserModel.findOne({name: this.model.name})
+      if (!uModel) {
+         this.model = new UserModel(this.model)
+         this.model.uid = this.#makeUid()
+         this.model.passwd = await this.generateHash(this.model.passwd)
+         if (!this.model.groups.includes('users')) this.model.groups.push('users')
+         return this.model.save().then(model => {
+            return model
+         })
+      } else {
+         this.model = uModel
+      }
+   }
 
-  async update(data) {
-    if (await UserModel.findOne({name: this.model.name})) {
-      return UserModel.findOneAndUpdate({name: this.model.name}, data).then(model => {
-        this.model = model
-        return model.save().then(model => {
-          return model
-        }).catch(err => {
-          throw err
-        })
-      }).catch(err => {
-        throw err
-      })
-    } else throw new Error('User not found')
-  }
+   async update(data) {
+      if (await UserModel.findOne({name: this.model.name})) {
+         return UserModel.findOneAndUpdate({name: this.model.name}, data).then(model => {
+            this.model = model
+            return model.save().then(model => {
+               return model
+            }).catch(err => {
+               throw err
+            })
+         }).catch(err => {
+            throw err
+         })
+      } else throw new Error('User not found')
+   }
 }
 
 function makeDefaultUser() {
-  const data = {name: 'root', groups: ['admin', 'users'], passwd: 'root'}
-  return new User(data)
+   const data = {name: 'root', groups: ['admin', 'users'], passwd: 'root'}
+   return new User(data)
 }
 
 function makeAccessToken(uid, groups) {
-  return jwt.sign({
-    u: uid, g: groups, d: Date.now()
-  }, certKeys.privkey, {expiresIn: `${env.APP_SESSION_EXPIRES}h`})
+   return jwt.sign({
+      u: uid, g: groups, d: Date.now()
+   }, certKeys.privkey, {expiresIn: `${env.APP_SESSION_EXPIRES}h`})
 }
 
-export { User, UserModel, makeDefaultUser, makeAccessToken }
+export {User, UserModel, makeDefaultUser, makeAccessToken}
